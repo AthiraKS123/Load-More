66b2be9305d67a0c5e6274f1e8578d76bfe1987b
AthiraKS123 <ksathira3062@gmail.com>
2024-04-27 18:55:50 +0530

    load more

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import LoadMore from './LoadMore';
+
+function App() {
+  return (
+    <div className="App">
+      <LoadMore />
+      
+    </div>
+  );
+}
+
+
+
+
+
+export default App;
